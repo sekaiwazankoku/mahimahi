137fc2b87ee876c035ec7ddd718b93c377f21f70
Harriet Chen <harrietchen070813@gmail.com>
2024-09-26 00:13:51 +0000

    Use millisecond timestamp
    To avoid any potential granularity and conversion problems

--- src/frontend/bbr_attack_queue.cc
+++ src/frontend/bbr_attack_queue.cc
@@ -15,7 +15,7 @@ BBRAttackQueue::BBRAttackQueue(
     const double attack_rate_,
     const uint64_t k_,
     const uint64_t delay_budget_)
-    : attack_rate(attack_rate_),
+    : attack_rate(attack_rate_), // Bytes per millisecond
       k(k_),
       delay_budget(delay_budget_),
       arrival_rate(0),
@@ -34,7 +34,7 @@ void BBRAttackQueue::detectState(Packet &p)
     if (!packet_queue_.empty())
     {
         Packet prev = packet_queue_.back();
-        current_arrival_rate = (double)p.contents.size() / (p.arrival_time - prev.arrival_time); // *1000 = Mbps
+        current_arrival_rate = (double)p.contents.size() / (p.arrival_time - prev.arrival_time);
 
         if (current_arrival_rate >= probe_gain * arrival_rate && state == CRUISE)
             state = PROBE;
@@ -64,16 +64,16 @@ void BBRAttackQueue::computeDelay(Packet &p)
 
 void BBRAttackQueue::read_packet(const string &contents)
 {
-    uint64_t now = timestamp_nano();
+    uint64_t now = timestamp();
     Packet p = {now, now, contents};
-    detectState(p);
+    // detectState(p);
     computeDelay(p);
     packet_queue_.emplace(p);
 }
 
 void BBRAttackQueue::write_packets(FileDescriptor &fd)
 {
-    while ((!packet_queue_.empty()) && (packet_queue_.front().dequeue_time <= timestamp_nano()))
+    while ((!packet_queue_.empty()) && (packet_queue_.front().dequeue_time <= timestamp()))
     {
         fd.write(packet_queue_.front().contents);
         packet_queue_.pop();
@@ -87,7 +87,7 @@ unsigned int BBRAttackQueue::wait_time(void) const
         return numeric_limits<uint16_t>::max();
     }
 
-    const auto now = timestamp_nano();
+    const auto now = timestamp();
 
     if (packet_queue_.front().dequeue_time <= now)
     {
